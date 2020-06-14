304c90540eb398497cc544e0dd49d843b250bed3
= <=>
2020-06-14 15:09:24 +0700

    Revert "commit to repository"
    This reverts commit ef0467819ad6cd9cccfb35cb0d3d7e2dfdf947f7.

--- admin/menu.php
+++
@@ -1,5 +0,0 @@
-<a href="./"> Trang chủ </a>
-<a href="./ListCongTy.php"> Quản lí công ty </a>
-<a href="./ListNganhNghe.php"> Quản lí ngành nghề </a>
-<a href="./ListCongViec.php"> Quản lí Công việc </a>
-<a href="./ListQuangCao.php"> Quản lí quảng cáo </a>
